b53442add5342a50c4ef38bcb318e8b15cce61a9
ekaardilahfebriyanti <ardilaheka@gmail.com>
2025-07-08 09:33:54 +0700

    init

---
+++ resources/lang/en/slider.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'slide_1_title' => 'Manage X, IG, WhatsApp, and More in One App',
+    'slide_1_desc' => 'Schedule posts, send WhatsApp messages, and boost engagement easily.',
+    'slide_1_cta' => 'Try the Demo',
+    'slide_2_cta' => 'Subscribe Now',
+
+    'slide_2_title' => 'Struggling to manage all your Social Media and WhatsApp accounts?',
+    'slide_2_desc' => 'Digimar helps you manage all social media and WhatsApp messages from one dashboard. No need to switch apps. All-in-one. You can even get help from AI.',
+
+    'slide_3_title' => 'tiktok.digimar.id - Maximize Your Business with TikTok Marketing',
+    'slide_3_desc' => 'Leverage the power of short videos to reach a wider audience. Boost brand awareness and sales with TikTok strategies powered by Digimar.',
+
+];
